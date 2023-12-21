27579cc46695a6e7be177fac1ab76a1a65cce5e5
Shuting Li <sli2@oxy.edu>
2023-12-20 23:26:24 -0500

    updated volunteer apointment accept/decline function

--- appointment_scheduler/scheduler_frontend/src/app/jwt-payload.model.ts
+++
@@ -1,9 +0,0 @@
-// src/app/jwt-payload.model.ts
-
-export interface JwtPayload {
-    sub: string;
-    name: string;
-    role: string;
-    // You can also include standard JWT fields like exp (expiration), iat (issued at), etc.
-    exp?: number;
-}
